c9f7fd85076ee1d8520f21c9333b10ff59740da9
Raj Kumar Shrestha <shresthar27@gmail.com>
2022-06-12 00:53:16 +0545

    added dynamic meta tags for projects

--- components/ProjectDescription.vue
+++ components/ProjectDescription.vue
@@ -20,23 +20,53 @@
   </div>
 </template>
 
-<script>
-export default {
-    props: ['project'],
-    setup(props) {
-        const year = computed(() => {
-            const date = props.project.date
-            return new Date(date).getFullYear();
-        })
-
-        const formattedURL = computed(() => {
-            const url = props.project.site
-            return url.replace('http://www.', '').replace('https://www.', '').toUpperCase()
-        })
-        
-        return { year, formattedURL }
-    }
+<script lang="ts" setup>
+/**
+ * Define props
+ */
+interface Props {
+  project: {
+      type: string;
+      [key: string]: any
+  }
 }
+const props = defineProps<Props>()
+
+const year = computed(() => {
+    const date = props.project.date
+    return new Date(date).getFullYear();
+})
+
+const formattedURL = computed(() => {
+    const url = props.project.site
+    return url.replace('http://www.', '').replace('https://www.', '').toUpperCase()
+})
+
+useHead({
+    title: `${props.project.title} Case Study`,
+    meta: [
+        {
+            name: 'description',
+            content: props.project.description
+        },
+        {
+            property: 'og:title',
+            content: `${props.project.title} Case Study`    
+        },
+        {
+            property: 'og:description',
+            content: props.project.description
+        },
+        {
+            name: 'twitter:title',
+            content: `${props.project.title} Case Study`    
+        },
+        {
+            name: 'twitter:description',
+            content: props.project.description
+        }
+    ]
+})
 </script>
 
 <style>
