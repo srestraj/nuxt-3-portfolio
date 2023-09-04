65b6b7d8ca60ca4d606fb33669593b6aa0d3e327
Raj Kumar Shrestha <kumarraj.sws@gmail.com>
2023-09-04 10:17:17 +0545

    made hero image dynamic by importing github avatar

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,16 +1,22 @@
 <template>
-  <section class="py-lg-5 py-4">
+  <section class="py-5">
       <div class="container">
-        <div class="row">
-          <div class="col-md-7 my-auto">
+        <div class="row gap-lg-0 gap-5 flex-lg-row flex-column-reverse">
+          <div class="col-lg-7 my-auto">
             <div>
                 <p class="hero-text">
                     <span>I’m Raj, a Frontend Developer.&nbsp;Always interested in Graphics, I started from a little coding knowledge, went on to designing stuff and later shifted to Frontend Development. Now, I'm a full-time Vue.js Developer.&nbsp;</span>
                 </p>
             </div>
           </div>
-          <div class="col-md-5">
-              <img src="~/assets/img/hero-img.png" alt="Raj Kumar Shrestha" class="img-fluid">
+          <div class="col-lg-5 position-relative">
+              <div class="rounded-md overflow-hidden w-100 img-container">
+                <img
+                  src="https://github.com/srestraj.png?size=512"
+                  alt="Raj Kumar Shrestha"
+                  class="img-fluid hero-img w-100 position-relative"
+                >
+              </div>
           </div>
         </div>
       </div>
